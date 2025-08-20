9e17dfe5892a42e73f344b0d6c7cd1f56466fd22
Rugved Somwanshi <rugved@lmstudio.ai>
2025-08-20 16:04:48 -0400

    Remove more boxing (bg) in texts and make error messages more consistent

--- src/logLevel.ts
+++ src/logLevel.ts
@@ -84,5 +84,6 @@ export function createLogger({ logLevel, verbose, quiet }: LogLevelArgs): Simple
       verbose === true || logLevel === "debug"
         ? undefined // If it is verbose, we use the default I
         : null, // Otherwise, no I
+    errorPrefix: chalk.red("Error:"),
   });
 }
